e0fbb0013ec11587a1d3fb13a763d495708a3b15
ksmith4134 <73805794+ksmith4134@users.noreply.github.com>
2022-10-05 10:06:17 -0400

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,13 @@
 
 
-export default function about() {
+export default function About() {
   return (
     <div className="mt-8 px-8">
         <div>
-           Expecting a different Kevin Smith? Don&apos;t worry, it happens all the time.
+           Expecting a different <a href="https://en.wikipedia.org/wiki/Kevin_Smith" target="_blank" rel="noreferrer" className="text-sky-500 hover:underline hover:underline-offset-4">Kevin Smith</a>? Don&apos;t worry, it happens all the time.
         </div>
         <div className="mt-6">
-            And while I may not be the <a href="https://en.wikipedia.org/wiki/Kevin_Smith" target="_blank" rel="noreferrer" className="text-sky-500 hover:underline hover:underline-offset-4">Kevin Smith</a> of acclaimed acting, directing, and podcasting fame &mdash; I am still glad you&apos;re here!
+            And while I may not be the Kevin Smith of acclaimed acting, directing, and podcasting fame &mdash; I am still glad you&apos;re here!
         </div>
         <div className="mt-6">
             Check out my portfolio, clone some open-source code, or just stay a while.
