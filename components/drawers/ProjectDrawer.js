65ef59c45bc4709b31d5b1541106dd8e268cef07
ksmith4134 <73805794+ksmith4134@users.noreply.github.com>
2022-10-12 16:19:16 -0400

    Update ProjectDrawer.js

--- components/drawers/ProjectDrawer.js
+++ components/drawers/ProjectDrawer.js
@@ -19,8 +19,8 @@ export default function ProjectDrawer(props) {
     return (
         <div className="bg-gray-900 bg-opacity-90 fixed top-0 left-0 w-screen h-screen z-50 text-black">
             <div className="absolute right-0 top-0 w-full max-w-screen-md bg-white dark:bg-neutral-900 dark:text-slate-50 h-full overflow-y-scroll">
-                <div className="px-12">
-                    <header className="pt-8 pb-4">
+                <div className="pt-10 pb-16 px-8 sm:px-16">
+                    <div className="text-center mx-auto">
                         <button onClick={() => closeDrawer()} className="absolute top-0 right-0 m-8 text-2xl cursor-pointer">
                             <FontAwesomeIcon icon={faTimes} className="" />
                         </button>
@@ -30,7 +30,7 @@ export default function ProjectDrawer(props) {
                             </div>
                         )}
                         <h2 className="mt-4 text-3xl font-bold">{project.entity}</h2>
-                        <h3 className="mt-2 text-lg">{project.title}</h3>
+                        <h3 className="mt-1 text-lg">{project.title}</h3>
 
                         {/* {project.type && (
                             <div className="mt-4 text-xs rounded-full bg-slate-600 text-white px-4 py-2 w-fit">
@@ -38,13 +38,14 @@ export default function ProjectDrawer(props) {
                             </div>
                         )} */}
 
-                        <div className="flex flex-wrap space-x-4 items-center mt-4">
+                        <div className="mt-6 flex flex-wrap gap-2 justify-center items-center">
                             {project.skills?.map((skill, index) => (
                                 <div key={index} className="text-xs px-4 py-2 bg-slate-300 dark:bg-slate-600 rounded-full">
                                     {skill.label}
                                 </div>
                             ))}
                         </div>
+                    </div>
 
                         <p className="mt-16 text-xl font-bold">App Summary</p>
                         <p className="mt-4 text-sm">{project.summary}</p>
@@ -79,7 +80,7 @@ export default function ProjectDrawer(props) {
                         )}
 
                         <p className="mt-12 text-xl font-bold">Tech Stack</p>
-                        <div className="flex items-center space-x-6 mt-4">
+                        <div className="flex flex-wrap items-center space-x-6 mt-4">
                             {project.tech?.map((e, index) => (
                                 <span key={index} className="">
                                     <Image src={e.logoSm} alt="tech logo" width={35} height={30} layout="fixed" />
@@ -89,7 +90,7 @@ export default function ProjectDrawer(props) {
                         
                         { project.contributions && (
                             <>
-                                <p className="mt-12 text-xl font-bold">Major Contributions</p>
+                                <p className="mt-12 text-xl font-bold">Notable Contributions</p>
                                 <ul>
                                     { project.contributions.map((contribution) => (
                                         <li key={contribution.id} className="mt-4 flex justify-start items-start gap-2">
@@ -102,7 +103,7 @@ export default function ProjectDrawer(props) {
                         )}
                         
 
-                    </header>
+                    
                 </div>
             </div>
         </div>
