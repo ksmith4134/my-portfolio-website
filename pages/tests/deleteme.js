cd301bfa8bff6c1f613673f4019c68ef732b403d
ksmith4134 <73805794+ksmith4134@users.noreply.github.com>
2022-10-11 20:55:03 -0400

    Update deleteme.js

--- pages/tests/deleteme.js
+++ pages/tests/deleteme.js
@@ -1,4 +1,4 @@
-import React from 'react'
+/* import React from 'react'
 
 export default function deleteme() {
   return (
@@ -20,3 +20,4 @@ export default function deleteme() {
     </div>
   )
 }
+ */
